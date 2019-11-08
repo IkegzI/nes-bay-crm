24b9d2366ca360b6cba9c16fd761250d0a5c6db4
IkegzI <kegz@oit-02-nettop.localdomain>
2019-11-08 18:17:12 +0800

    work on views contacts v1

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,16 +1,33 @@
 class ContactController < ApplicationController
   def new
+    Contact.new
   end
 
   def create
+    @contact = Contact.new()
   end
 
   def index
+    @contact = Contact.all
   end
 
   def show
+    @contact = Contact.find(params_id)
   end
 
   def destroy
   end
+
+  private
+
+  def params_id
+    params.require(:id).to_i
+  end
+
+  def params_contact
+    param = {}
+    params.require(:contact).each { |item| param[item[0].to_sym] = item[1] }
+    return param
+  end
+
 end
